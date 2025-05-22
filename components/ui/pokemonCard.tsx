c15827a74fa6495f126b0f35d7dfdc045b216dac
4LanCrane <125448543+4LanCrane@users.noreply.github.com>
2025-05-22 02:51:38 +0100

    formating pokemonCard

--- components/ui/pokemonCard.tsx
+++ components/ui/pokemonCard.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function PokemonCard({ name, imageUrl }: { name: string; imageUrl: string }) {
   return (
-    <div className="shadow-sm rounded-xs flex flex-col items-center justify-center w-50 h-50">
+    <div className="shadow-sm rounded-xs flex flex-col  w-50 h-50">
       <div className="flex items-center justify-center w-full h-full bg-gray-200 ">
         <img src={imageUrl} alt={name} className="w-16 h-16" />
       </div>
-      <div className="">
-        <h1 className="text-2xl font-bold">Item</h1>
-        <p className="text-lg">{name}</p>
+      <div className="float-left ml-3 ">
+        <h1 className="font-semibold">{name}</h1>
+        <p className="text-gray-500 text-sm">test</p>
       </div>
     </div>
   );
