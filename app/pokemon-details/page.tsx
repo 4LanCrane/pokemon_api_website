bba0941281d2c7ceda0f028e7437dfaa340cb396
4LanCrane <125448543+4LanCrane@users.noreply.github.com>
2025-05-24 19:32:30 +0100

    implementing Grid for details page

--- app/pokemon-details/page.tsx
+++ app/pokemon-details/page.tsx
@@ -43,50 +43,86 @@ export default function PokemonDetails() {
         </p>
       </div>
 
-      <div className="w-full max-w-md mx-auto mt-6 space-y-3 bg-amber-300">
-        <div className="flex items-center justify-between">
-          <p>HP</p>
-          <Progress value={pokemon.stats[0].base_stat} className="w-72 mt-2" />
+      {/* Grid starts here */}
+      <div className="grid  gap-1   ">
+        {/* this is the top span box*/}
+        <div className="col-start-1 row-start-1 col-span-3 bg-amber-400 h-20"></div>
+
+    {/*this is the large sidebar */}
+        <div className="bg-green-600 col-start-1 row-span-3 col-span-1 ">
+        <p>testing</p>
         </div>
 
-        <div className="flex items-center justify-between">
-          <p>Attack</p>
-          <Progress value={pokemon.stats[1].base_stat} className="w-72 mt-2" />
+        <div className="col-start-2 row-start-2 col-span-1 bg-gray-800 p-4 rounded-lg shadow-md">
+          <p>
+            <strong>Types:</strong>
+            {pokemon.types.map((t: any) => t.type.name).join(", ")}
+          </p>
+          <p>
+            <strong>Weaknesses:</strong>
+            {pokemon.types.map((t: any) => t.type.name).join(", ")}
+          </p>
         </div>
 
-        <div className="flex items-center justify-between">
-          <p>Defense</p>
-          <Progress value={pokemon.stats[2].base_stat} className="w-72 mt-2" />
+        <div className="col-span-1 col-start-3 bg-gray-800 p-4 rounded-lg shadow-md">
+          <p>
+            <strong>Abilities:</strong>
+            {pokemon.abilities.map((a: any) => a.ability.name).join(", ")}
+          </p>
         </div>
 
-        <div className="flex items-center justify-between">
-          <p>Special Attack</p>
-          <Progress value={pokemon.stats[3].base_stat} className="w-72 mt-2" />
-        </div>
+        {/* This is the stats for the pokemon */}
+        <div className=" bg-amber-300 col-start-2 col-span-2 row-start-3">
+          <div className="flex items-center justify-between">
+            <p>HP</p>
+            <Progress
+              value={pokemon.stats[0].base_stat}
+              className="w-72 mt-2"
+            />
+          </div>
 
-        <div className="flex items-center justify-between">
-          <p>Special Defense</p>
-          <Progress value={pokemon.stats[4].base_stat} className="w-72 mt-2" />
-        </div>
+          <div className="flex items-center justify-between">
+            <p>Attack</p>
+            <Progress
+              value={pokemon.stats[1].base_stat}
+              className="w-72 mt-2"
+            />
+          </div>
 
-        <div className="flex items-center justify-between">
-          <p>Speed</p>
-          <Progress value={pokemon.stats[5].base_stat} className="w-72 mt-2" />
+          <div className="flex items-center justify-between">
+            <p>Defense</p>
+            <Progress
+              value={pokemon.stats[2].base_stat}
+              className="w-72 mt-2"
+            />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <p>Special Attack</p>
+            <Progress
+              value={pokemon.stats[3].base_stat}
+              className="w-72 mt-2"
+            />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <p>Special Defense</p>
+            <Progress
+              value={pokemon.stats[4].base_stat}
+              className="w-72 mt-2"
+            />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <p>Speed</p>
+            <Progress
+              value={pokemon.stats[5].base_stat}
+              className="w-72 mt-2"
+            />
+          </div>
         </div>
       </div>
 
-      <p>
-        <strong>Abilities:</strong>
-        {pokemon.abilities.map((a: any) => a.ability.name).join(", ")}
-      </p>
-      <p>
-        <strong>Types:</strong>
-        {pokemon.types.map((t: any) => t.type.name).join(", ")}
-      </p>
-      <p>
-        <strong>Weaknesses:</strong>
-        {pokemon.types.map((t: any) => t.type.name).join(", ")}
-      </p>
       <a href="/" className="text-blue-600 underline mt-4 block">
         Back to list
       </a>
